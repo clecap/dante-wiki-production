c90383d4da62ec9a1751d7e52a7944f542e39308
clecap <clemens.cap@uni-rostock.de>
2023-11-06 21:41:53 +0100

    MP:

--- update-dante.sh
+++ update-dante.sh
@@ -58,7 +58,7 @@ function getting () {
   printf "DONE copying in backup of configuration file\n\n"
 }
 
-printf "\n\n\n Dante Updater Version 2.10 "
+printf "\n\n\n Dante Updater Version 2.11 "
 
 backup
 
