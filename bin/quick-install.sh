82f656ce4e15488d306624dff5ebdc563c2fcf1d
clecap <clemens.cap@uni-rostock.de>
2023-11-14 13:26:15 +0100

    MP:

--- bin/quick-install.sh
+++ bin/quick-install.sh
@@ -3,9 +3,14 @@
 MAIN_DIR=dante
 BRANCH=master
 REPO=dante-wiki-production
+VERISON=1.1
+
+printf "\n"
+printf "**************************\n"
+printf "*** QUICK INSTALLER ${VERSION} ***\n"
+printf "**************************\n"
 
 cd ~dante
-
 wget https://github.com/clecap/${REPO}/archive/refs/heads/${BRANCH}.zip
 unzip ${BRANCH}.zip
 cd ${REPO}-${BRANCH}
