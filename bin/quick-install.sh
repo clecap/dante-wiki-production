a60bc4a6c5b24fd3bc5327a314d93a9f61201c94
clecap <clemens.cap@uni-rostock.de>
2024-01-27 14:19:37 +0100

    MP:

--- bin/quick-install.sh
+++ bin/quick-install.sh
@@ -10,7 +10,8 @@ printf "***************************\n"
 printf "*** QUICK INSTALLER ${VERSION} ***\n"
 printf "***************************\n"
 
-read -p "We will attempt to delete an old installation in ${PWD}/${MAIN_DIR} " -n 1 -r
+echo "We will attempt to delete an old installation in ${PWD}/${MAIN_DIR} "
+read -p "Press  y  if this is okay." -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
